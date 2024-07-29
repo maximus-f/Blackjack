4a14e5968c0872de40b0b0c5cb8e0bf3108fce39
perotins <maxfuligni@gmail.com>
2024-07-28 22:55:12 -0400

    1.7.4 - add BlackjackExpansion.java for PAPI

---
+++ src/main/java/me/perotin/blackjack/util/BlackjackExpansion.java
@@ -0,0 +1,21 @@
+package at.helpch.placeholderapi.example.expansion;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+
+public class BlackjackExpansion extends PlaceholderExpansion {
+
+    @Override
+    public String getAuthor() {
+        return "Perotin"; //
+    }
+
+    @Override
+    public String getIdentifier() {
+        return "Blackjack"; //
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.7.4"; //
+    }
+}
